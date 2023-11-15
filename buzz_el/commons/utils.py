f0b84ac67219cf38ed5fc44d11ecf729982404a9
msesboue <46442957+msesboue@users.noreply.github.com>
2023-11-15 08:44:38 +0100

    Feat/4 rdf graph loader (#5)
    Feat: RDF graph loader

---
+++ buzz_el/commons/utils.py
@@ -0,0 +1,22 @@
+from urllib.parse import urlparse
+
+
+def is_valid_url(uri):
+    """Test if a string is a URL or not.
+
+    Parameters
+    ----------
+    uri : str
+        The string to test.
+
+    Returns
+    -------
+    bool
+        Whether the string is a URL.
+    """
+    try:
+        result = urlparse(uri)
+        # Check if the scheme component is present
+        return all([result.scheme, result.netloc])
+    except ValueError:
+        return False
